174e314f98fd88859371f7325e041acd3b547c63
Massimo La Sorsa <futbolyapp@gmail.com>
2025-07-10 23:55:16 +0100

    Rimossa regola TypeScript da ESLint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['react-app'],
   rules: {
     'no-unused-vars': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn'
+    'react-hooks/exhaustive-deps': 'warn'
   }
 }; 
